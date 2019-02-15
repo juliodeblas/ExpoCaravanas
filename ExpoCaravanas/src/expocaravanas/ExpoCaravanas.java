c683e9a82cd9199ba4ba135af4c2e5b5faca4dd0
Julio de Blas <julio.deblas@cesjuanpablosegundo.es>
2019-02-15 12:18:25 +0100

    ExpoCaravanas3

--- ExpoCaravanas/src/expocaravanas/ExpoCaravanas.java
+++ ExpoCaravanas/src/expocaravanas/ExpoCaravanas.java
@@ -28,42 +28,49 @@ public class ExpoCaravanas {
         Zona zonavip = new Zona();
         zonavip.setEntradas(25);
 
-        System.out.println("1-------------->Mostrar entradas libres");
-        System.out.println("2-------------->Comprar entradas");
-        System.out.println("3-------------->Salir");
         opcion1 = 1;
 
         while (opcion1 != 3) {
-
+            System.out.println("1-------------->Mostrar entradas libres");
+            System.out.println("2-------------->Comprar entradas");
+            System.out.println("3-------------->Salir");
             System.out.println("¿Qué opción quieres realizar?");
             opcion1 = sc.nextInt();
+            System.out.println("");
             if (opcion1 == 1) {
                 System.out.println("Quedan " + zonap.getEntradas() + " en la zona principal.");
                 System.out.println("Quedan: " + zonacv.getEntradas() + " en la zona de compra-venta.");
                 System.out.println("Quedan: " + zonavip.getEntradas() + " en la zona VIP.");
+                System.out.println("");
             } else if (opcion1 == 2) {
                 System.out.println("¿En qué zona quieres estar?");
                 System.out.println("1-------------->ZONA PRINCIPAL");
                 System.out.println("2-------------->ZONA COMPRA-VENTA");
                 System.out.println("3-------------->ZONA VIP");
                 opcion2 = sc.nextInt();
+                System.out.println("");
                 if (opcion2 == 1) {
                     System.out.println("¿Cuántas entradas quieres comprar?");
                     entradaspedidas = sc.nextInt();
                     zonap.Venta(entradaspedidas);
+                    System.out.println("");
                 } else if (opcion2 == 2) {
                     System.out.println("¿Cuántas entradas quieres comprar?");
                     entradaspedidas = sc.nextInt();
                     zonacv.Venta(entradaspedidas);
+                    System.out.println("");
                 } else if (opcion2 == 3) {
                     System.out.println("¿Cuántas entradas quieres comprar?");
                     entradaspedidas = sc.nextInt();
                     zonavip.Venta(entradaspedidas);
+                    System.out.println("");
                 } else {
                     System.out.println("ERROR");
+                    System.out.println("");
                 }
             } else if (opcion1 != 1 && opcion1 != 2 && opcion1 != 3) {
                 System.out.println("ERROR");
+                System.out.println("");
             }
         }
     }
